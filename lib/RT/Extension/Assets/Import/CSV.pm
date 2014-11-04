332b10f408e5d5ea5878b0a3401cded4d7d3824f
Kevin Falcone <falcone@bestpractical.com>
2014-11-03 21:16:10 -0500

    Bump version for 1.4

--- lib/RT/Extension/Assets/Import/CSV.pm
+++ lib/RT/Extension/Assets/Import/CSV.pm
@@ -4,7 +4,7 @@ use warnings;
 package RT::Extension::Assets::Import::CSV;
 use Text::CSV_XS;
 
-our $VERSION = '1.3';
+our $VERSION = '1.4';
 
 sub _column {
     ref($_[0]) ? (ref($_[0]) eq "CODE" ?
